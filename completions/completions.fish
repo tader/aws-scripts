9da3af53a0f1f399626ac82b1e17952366a57fab
Thomas A. de Ruiter <thomas@de-ruiter.cx>
2019-05-24 12:36:38 +0000

    Add more scripts and fish shell completions

---
+++ completions/completions.fish
@@ -0,0 +1,2 @@
+complete -x -d "CloudFormation Stack" -c tail-cloudformation-events          -a '(list-cloudformation-stacks)'
+complete -x -d "CloudFormation Stack" -c list-cloudformation-stack-resources -a '(list-cloudformation-stacks)'

54d244d1a7d383c3e2c7f67918e207e46b3a27fa
Thomas de Ruiter <thomasderuiter@binx.io>
2019-11-01 13:43:52 +0100

    add iam user reset

--- completions/completions.fish
+++ completions/completions.fish
@@ -1,2 +1,4 @@
 complete -x -d "CloudFormation Stack" -c tail-cloudformation-events          -a '(list-cloudformation-stacks)'
 complete -x -d "CloudFormation Stack" -c list-cloudformation-stack-resources -a '(list-cloudformation-stacks)'
+complete -x -d "IAM User" -c list-iam-mfa-devices -a '(list-iam-users)'
+complete -x -d "IAM User" -c reset-iam-user -a '(list-iam-users)'
